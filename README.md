296ad9d45cde2162a58d516328fa5d5905a50354
Juuxel <kasperi.kauppi@gmail.com>
2019-09-02 21:21:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<img src="src/main/resources/assets/beeangry-est/icon.png" align="left" width="180px"/>
+
+# Adorn
+
+[![](https://img.shields.io/github/license/Juuxel/Adorn.svg)](LICENSE) [![](https://img.shields.io/github/release/Juuxel/BeeAngry-est.svg)](https://github.com/Juuxel/BeeAngry-est/releases) ![](https://img.shields.io/badge/minecraft-1.15-blueviolet.svg)
+
+*Makes bees angrier!* • [Downloads](https://github.com/Juuxel/Adorn/releases)
---
+++ src/main/java/juuxel/bee/mixin/BeeEntityMixin.java
@@ -0,0 +1,36 @@
+package juuxel.bee.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BeeEntity.class)
+public abstract class BeeEntityMixin extends AnimalEntity {
+    @Unique
+    private static final float BEE_EXPLOSION_STRENGTH = 2.3f;
+
+    protected BeeEntityMixin(EntityType<? extends AnimalEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tryAttack", at = @At("RETURN"))
+    private void onTryAttack(Entity entity, CallbackInfoReturnable<Boolean> info) {
+        if (info.getReturnValueZ()) {
+            world.createExplosion(this, x, y, z, BEE_EXPLOSION_STRENGTH, Explosion.DestructionType.NONE);
+        }
+    }
+
+    @Inject(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/BeeEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 0))
+    private void onMobTick(CallbackInfo info) {
+        world.createExplosion(this, x, y, z, BEE_EXPLOSION_STRENGTH, Explosion.DestructionType.DESTROY);
+    }
+}
